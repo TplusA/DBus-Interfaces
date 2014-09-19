5903513221b879e88f7eabdae645bc84629eb929
Robert Tiemann <r.tiemann@ta-hifi.com>
2014-09-19 12:28:08 +0200

    Collection of D-Bus interfaces used in the SCL project.
    This first commit adds a script for generating Doxygen documentation
    from the D-Bus introspection files.

---
+++ extract_documentation.py
@@ -0,0 +1,280 @@
+#! /usr/bin/env python
+
+from __future__ import print_function
+
+import sys
+import getopt
+import re
+import xml.dom.minidom
+
+def error_exit(error_message, exit_code = 1):
+    print("Error: " + error_message, file = sys.stderr)
+    sys.exit(exit_code)
+
+class CommentedElement:
+    def __init__(self, node, comment):
+        self.node = node
+        self.comment_lines = CommentedElement.format_comment(comment)
+
+    def get_comment(self, prefix = ""):
+        return "\n".join(map(lambda x: (prefix + x).rstrip(), self.comment_lines))
+
+    @staticmethod
+    def format_comment(comment):
+        if not comment:
+            return None
+
+        lines = comment.split("\n")
+        indentation = 80
+
+        for line in lines:
+            stripped_len = len(line.lstrip())
+            if stripped_len > 0:
+                indentation = min(len(line) - stripped_len, indentation)
+
+        empty_lines = 0
+        dest_lines = []
+
+        for i in range(len(lines)):
+            temp = lines[i][indentation : ]
+
+            if len(temp.lstrip()) == 0:
+                empty_lines += 1
+            else:
+                if empty_lines and len(dest_lines) > 0:
+                    dest_lines.append("")
+
+                dest_lines.append(temp)
+                empty_lines = 0
+
+        return dest_lines
+
+def get_elements_with_comments(nodes):
+    elements = []
+    current_comment = None
+
+    for n in nodes:
+        if n.nodeType == xml.dom.minidom.Node.TEXT_NODE:
+            continue
+
+        if n.nodeType == xml.dom.minidom.Node.COMMENT_NODE:
+            current_comment = n.data
+        else:
+            elements.append(CommentedElement(n, current_comment))
+            current_comment = None
+
+    return elements
+
+def concat_c_names(first, second):
+    return first + "_" + camel_case_to_c_style(second)
+
+def camel_case_to_c_style(cc):
+    return re.sub(r"([^A-Z_])([A-Z])", r"\1_\2", cc).lower()
+
+def ifacename_to_cstyle(iface_name):
+    return camel_case_to_c_style(iface_name.replace(".", "_"))
+
+def ifacename_to_refname(kind, iface_name):
+    return "dbus_" + kind + "_" + ifacename_to_cstyle(iface_name)
+
+def write_section(section_name, verb, iface_name, f):
+    print("\n\\section " + ifacename_to_refname(section_name.lower(), iface_name) + " " + section_name, file = f)
+    print("\nHere is a list of " + section_name.lower() + " " + verb + " by the <tt>" + iface_name + "</tt> interface:\n", file = f)
+
+def write_table(kind, prefix, members, mdfile):
+    table_rows = [[kind + " name", "Related function"]]
+
+    for m in members:
+        name = m.node.getAttribute("name")
+        table_rows.append(["<tt>" + name + "</tt>", "#" + concat_c_names(prefix, name) + "()"])
+
+    col1_width = 0
+    col2_width = 0
+    for row in table_rows:
+        col1_width = max(len(row[0]), col1_width)
+        col2_width = max(len(row[1]), col2_width)
+
+    format_string = "{:<" + str(col1_width) + "} | {:}"
+
+    row = table_rows.pop(0)
+    print(format_string.format(row[0], row[1]), file = mdfile)
+    print("-" * (col1_width + 1) + "|" + "-" * (col2_width + 1), file = mdfile)
+
+    for row in table_rows:
+        print(format_string.format(row[0], row[1]), file = mdfile)
+
+dbus_type_to_ctype = {
+    "b": "gboolean",
+    "d": "gdouble",
+    "i": "gint",
+    "n": "gint16",
+    "q": "guint16",
+    "s": "const gchar *",
+    "x": "gint64"
+}
+
+def generate_specific_parameter_list(parameters, args):
+    for arg in args:
+        name = arg.getAttribute("name")
+        dbus_type = arg.getAttribute("type")
+
+        if dbus_type in dbus_type_to_ctype:
+            c_type = dbus_type_to_ctype[dbus_type]
+        elif len(dbus_type) > 1:
+            c_type = "GVariant *"
+        else:
+            error_exit('Unsupported D-Bus type "' + dbus_type + '" used for argument "' + name + '"')
+
+        parameters.append(c_type + " arg_" + name)
+
+def generate_signal_emit_parameter_list(proxy_typename, arguments):
+    args = arguments.getElementsByTagName("arg")
+
+    parameters = []
+    parameters.append(proxy_typename + " *object")
+    generate_specific_parameter_list(parameters, args)
+
+    return ", ".join(parameters)
+
+def generate_method_call_parameter_list(proxy_typename, arguments):
+    args = arguments.getElementsByTagName("arg")
+
+    parameters = []
+    parameters.append(proxy_typename + " *proxy")
+    generate_specific_parameter_list(parameters, args)
+    parameters.append("GCancellable *cancellable")
+    parameters.append("GAsyncReadyCallback callback")
+    parameters.append("gpointer user_data")
+
+    return ", ".join(parameters)
+
+def generate_proxy_typename(c_namespace, iface_name):
+    return re.sub(r"_", r"", c_namespace) + iface_name;
+
+def write_documentation(c_namespace, prefix, proxy_typename, generate_parameter_list, members, hfile):
+    for m in members:
+        if not m.comment_lines:
+            continue
+
+        print(r"/*!", file = hfile)
+
+        name = m.node.getAttribute("name")
+        print(r" * \fn void " + concat_c_names(prefix, name) + "(" + generate_parameter_list(proxy_typename, m.node) + ")", file = hfile)
+        print(r" *", file = hfile)
+
+        print(m.get_comment(r" * "), file = hfile)
+
+        print(r" */", file = hfile)
+
+def strip_prefix_from_interface_name(iface_name, strip_prefix):
+    if iface_name.startswith(strip_prefix):
+        iface_name = iface_name[len(strip_prefix) : ]
+
+    return iface_name
+
+def generate_files_from_interfaces(interfaces, component_name, c_namespace, strip_prefix, hfile, mdfile):
+    print(r"""\page dbus_interfaces D-Bus interface documentation
+
+Interfaces of _""" + component_name + "_:", file = mdfile)
+
+    for iface in interfaces:
+        print(r"- \subpage " + ifacename_to_refname("iface", iface.node.getAttribute("name")), file = mdfile)
+
+    for iface in interfaces:
+        if iface.node.nodeType != xml.dom.minidom.Node.ELEMENT_NODE:
+            error_exit("Unexpected node type " + str(iface.nodeType))
+
+        if iface.node.tagName != "interface":
+            error_exit('Unexpected tag "' + iface.node.tagName + '", expected interface')
+
+        members = get_elements_with_comments(iface.node.childNodes)
+        all_signals = [m for m in members if m.node.tagName == "signal"]
+        all_methods = [m for m in members if m.node.tagName == "method"]
+        all_properties = [m for m in members if m.node.tagName == "property"]
+
+        iface_name = iface.node.getAttribute("name")
+        fnname_prefix = ifacename_to_cstyle(strip_prefix_from_interface_name(iface_name, strip_prefix))
+        if c_namespace:
+            fnname_prefix = c_namespace + "_" + fnname_prefix
+
+        print("\n\n\\page " + ifacename_to_refname("iface", iface_name) + " Interface " + iface_name, file = mdfile)
+
+        if iface.comment_lines:
+            print("\n" + iface.get_comment(), file = mdfile)
+
+        proxy_typename = generate_proxy_typename(c_namespace, strip_prefix_from_interface_name(iface_name, strip_prefix))
+
+        if len(all_signals) > 0:
+            write_section("Signals", "emitted", iface_name, mdfile)
+            write_table("Signal", fnname_prefix + "_emit", all_signals, mdfile)
+            write_documentation(c_namespace, fnname_prefix + "_emit", proxy_typename, generate_signal_emit_parameter_list, all_signals, hfile)
+
+        if len(all_methods) > 0:
+            write_section("Methods", "implemented", iface_name, mdfile)
+            write_table("Method", fnname_prefix + "_call", all_methods, mdfile)
+            write_documentation(c_namespace, fnname_prefix + "_call", proxy_typename, generate_method_call_parameter_list, all_methods, hfile)
+
+        if len(all_properties) > 0:
+            write_section("Properties", "exposed", iface_name, mdfile)
+            write_table("Property", "FIXME", all_properties, iface_name, mdfile)
+            error_exit("Exporting properties documentation is not correctly implemented")
+
+
+def usage(exit_code = 1):
+    print(r"""\
+Usage: """ + sys.argv[0] + r""" -i xmlfile -H hfile -o mdfile -n name [-c ns] [-s strip] [-h]
+
+Options:
+-i file  Input XML file containing D-Bus introspection data.
+-H file  Output header file for Doxygen containing only comments for functions
+         supposedly generated by gdbus-codegen. The comments, if any, are
+         extracted from the introspection XML file.
+-o file  Output Markdown file for Doxygen containing the documentation pages
+         for D-Bus interfaces defined in the introspection XML file.
+-n str   Name of the software component as it should appear in the
+         documentation.
+-c str   C namespace prefix for generated C names.
+-s str   Prefix to remove from the interface names for generated C names.
+-h       This help screen.""", file = sys.stderr)
+    sys.exit(exit_code)
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "c:i:hH:n:o:s:")
+    except getopt.GetoptError as err:
+        error_exit(err)
+
+    xml_input_file = None
+    doxygen_markdown_file = None
+    doxygen_header_file = None
+    component_name = None
+    c_namespace = ""
+    strip_from_interface_name = ""
+
+    for o, a in opts:
+        if o == "-h":   usage(0)
+        elif o == '-c': c_namespace = a
+        elif o == "-i": xml_input_file = a
+        elif o == "-H": doxygen_header_file = a
+        elif o == '-n': component_name = a
+        elif o == "-o": doxygen_markdown_file = a
+        elif o == '-s': strip_from_interface_name = a
+
+    if not xml_input_file or not doxygen_header_file or not doxygen_markdown_file or not component_name:
+        usage()
+
+    xmldoc = xml.dom.minidom.parse(xml_input_file)
+
+    node_def = xmldoc.getElementsByTagName("node")
+    if len(node_def) != 1:
+        error_exit("Found " + str(len(node_def)) + " node elements.")
+
+    interfaces = get_elements_with_comments(node_def[0].childNodes)
+    if len(interfaces) == 1:
+        error_exit("No interfaces defined in " + xml_input_file)
+
+    with open(doxygen_header_file, "w+") as hfile, open(doxygen_markdown_file, "w+") as mdfile:
+        generate_files_from_interfaces(interfaces, component_name, c_namespace, strip_from_interface_name, hfile, mdfile)
+
+if __name__ == "__main__":
+    main()
