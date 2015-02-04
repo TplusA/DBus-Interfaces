d709f6397d6373d2f62404c49becc58e4de1ee2a
Robert Tiemann <r.tiemann@ta-hifi.com>
2015-02-04 10:57:58 +0100

    Apply GPLv3 to source code.
    Change-Id: If025b3d3839bea39332b26fe11907a11b4d35d55

---
+++ README.md
@@ -0,0 +1,14 @@
+# D-Bus interfaces for T+A Streaming Board software
+
+## Copyright and contact
+
+The T+A Streaming Board D-Bus interfaces (T+A-D-Bus) are released under the
+terms of the GNU General Public License version 3 (GPLv3). See file
+<tt>COPYING</tt> for licensing terms.
+
+Contact:
+
+    T+A elektroakustik GmbH & Co. KG
+    Planckstrasse 11
+    32052 Herford
+    Germany
--- extract_documentation.py
+++ extract_documentation.py
@@ -1,5 +1,23 @@
 #! /usr/bin/env python
 
+#
+# Copyright (C) 2015  T+A elektroakustik GmbH & Co. KG
+#
+# This file is part of T+A-D-Bus.
+#
+# T+A-D-Bus is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 3 as
+# published by the Free Software Foundation.
+#
+# T+A-D-Bus is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with T+A-D-Bus.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 from __future__ import print_function
 
 import sys
