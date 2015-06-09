25f08319c96b15c06e3dcf96aed28809922771d2
Robert Tiemann <r.tiemann@ta-hifi.com>
2015-06-09 17:17:08 +0200

    Header files for de.tahifi.Lists.Navigation error codes.
    It is very helpful to be able to see the actual cause of any errors
    returned by the lists-related D-Bus methods. This way the caller
    roughly knows what's going on and can do something to help the user
    resolving problems.
    
    To make it work, appropriate error codes must be determined at the
    point of failure, and these need to bubble all the way up to the D-Bus
    adapter code (or whoever called). This can be done either by return
    codes, output parameters, or exceptions, whatever fits best.
    
    For C, a header file containing a plain enum and full documentation of
    error codes is provided. For C++, a small class based on the C header
    file is provided. A moderate amount of macro magic is invoked to make
    the enum names acceptable for C and C++, respectively.
    
    The Documentation in the XML file points to the header files and does
    not attempt to document the error codes any further.
    
    Change-Id: Iafc380b2cde84755c218edf237e59b305ed84725

---
+++ de_tahifi_lists_errors.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2015  T+A elektroakustik GmbH & Co. KG
+ *
+ * This file is part of T+A Streaming Board D-Bus interfaces (T+A-D-Bus).
+ *
+ * T+A-D-Bus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 3 as
+ * published by the Free Software Foundation.
+ *
+ * T+A-D-Bus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with T+A-D-Bus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef DE_TAHIFI_LISTS_ERRORS_H
+#define DE_TAHIFI_LISTS_ERRORS_H
+
+#ifndef DE_TAHIFI_LISTS_ERRORS_ENUM_NAME
+#define DE_TAHIFI_LISTS_ERRORS_ENUM_NAME        DBusListsErrorCode
+#endif /* !DE_TAHIFI_LISTS_ERRORS_ENUM_NAME */
+
+#ifndef DE_TAHIFI_LISTS_ERRORS_ENUM_VALUE
+#define DE_TAHIFI_LISTS_ERRORS_ENUM_VALUE(V)    LIST_ERROR_ ## V
+#endif /* !DE_TAHIFI_LISTS_ERRORS_ENUM_PREFIX */
+
+enum DE_TAHIFI_LISTS_ERRORS_ENUM_NAME
+{
+    /*! No error occurred. */
+    DE_TAHIFI_LISTS_ERRORS_ENUM_VALUE(OK) = 0U,
+
+    /*! An internal error occurred (a bug). */
+    DE_TAHIFI_LISTS_ERRORS_ENUM_VALUE(INTERNAL),
+
+    /*! The ID passed into the D-Bus method was invalid. */
+    DE_TAHIFI_LISTS_ERRORS_ENUM_VALUE(INVALID_ID),
+
+    /*! I/O error on physical medium (e.g., read error on some USB drive). */
+    DE_TAHIFI_LISTS_ERRORS_ENUM_VALUE(PHYSICAL_MEDIA_IO),
+
+    /*! I/O error on the network (e.g., broken network connection). */
+    DE_TAHIFI_LISTS_ERRORS_ENUM_VALUE(NET_IO),
+
+    /*! Network protocol error. */
+    DE_TAHIFI_LISTS_ERRORS_ENUM_VALUE(PROTOCOL),
+
+    /*! Authentication with some external system has failed. */
+    DE_TAHIFI_LISTS_ERRORS_ENUM_VALUE(AUTHENTICATION),
+};
+
+#undef DE_TAHIFI_LISTS_ERRORS_ENUM_NAME
+#undef DE_TAHIFI_LISTS_ERRORS_ENUM_VALUE
+
+#endif /* !DE_TAHIFI_LISTS_ERRORS_H */
---
+++ de_tahifi_lists_errors.hh
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2015  T+A elektroakustik GmbH & Co. KG
+ *
+ * This file is part of T+A Streaming Board D-Bus interfaces (T+A-D-Bus).
+ *
+ * T+A-D-Bus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 3 as
+ * published by the Free Software Foundation.
+ *
+ * T+A-D-Bus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with T+A-D-Bus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef DE_TAHIFI_LISTS_ERRORS_HH
+#define DE_TAHIFI_LISTS_ERRORS_HH
+
+#include <inttypes.h>
+
+class ListError
+{
+  public:
+#define DE_TAHIFI_LISTS_ERRORS_ENUM_NAME        Code
+#define DE_TAHIFI_LISTS_ERRORS_ENUM_VALUE(V)    V
+#include "de_tahifi_lists_errors.h"
+
+  private:
+    Code error_code_;
+
+  public:
+    constexpr explicit ListError(Code error_code = Code::OK) throw():
+        error_code_(error_code)
+    {}
+
+    uint8_t get_raw_code() const noexcept { return uint8_t(error_code_); }
+
+    ListError &operator=(Code code) noexcept
+    {
+        error_code_ = code;
+        return *this;
+    }
+
+    bool operator==(Code code) const noexcept
+    {
+        return error_code_ == code;
+    }
+
+    bool operator!=(Code code) const noexcept
+    {
+        return !(*this == code);
+    }
+
+    bool operator==(const ListError &other) const noexcept
+    {
+        return *this == other.error_code_;
+    }
+
+    bool failed() const noexcept
+    {
+        return *this != Code::OK;
+    }
+};
+
+template <typename T>
+static T& operator<<(T& os, const ::ListError &error)
+{
+    /* must be sorted according to #ListError::Code enum */
+    static constexpr const char *names[] =
+    {
+        "OK",
+        "INTERNAL",
+        "INVALID_ID",
+        "PHYSICAL_MEDIA_IO",
+        "NET_IO",
+        "PROTOCOL",
+        "AUTHENTICATION",
+    };
+
+    static constexpr char prefix[] = "ListError::Code::";
+
+    if(error.get_raw_code() < sizeof(names) / sizeof(names[0]))
+        os << prefix << names[error.get_raw_code()];
+    else
+        os << "***Invalid ListError code***";
+
+    return os;
+}
+
+#endif /* !DE_TAHIFI_LISTS_ERRORS_HH */
