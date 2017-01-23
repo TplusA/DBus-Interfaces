8b97623e2f9b331fc16b1c7773cf5c753e1f40ae
Robert Tiemann <r.tiemann@ta-hifi.com>
2017-01-23 17:12:24 +0100

    Interfaces for interacting with a cover art management service.
    Change-Id: Ie55f8fd325bd097e8595586739f7ad009216893c

--- test_doxygen.sh
+++ test_doxygen.sh
@@ -10,6 +10,7 @@ DOCEXTRACT='../extract_documentation.py'
 
 $CODEGEN --generate-c-code=upnp_dleynaserver_dbus --c-namespace tdbus_dleynaserver --interface-prefix com.intel.dLeynaServer. ../com_intel_dleynaserver.xml
 $CODEGEN --generate-c-code=airable_dbus           --c-namespace tdbus              --interface-prefix de.tahifi.              ../de_tahifi_airable.xml
+$CODEGEN --generate-c-code=artcache_dbus          --c-namespace tdbus_artcache     --interface-prefix de.tahifi.ArtCache.     ../de_tahifi_artcache.xml
 $CODEGEN --generate-c-code=configuration_dbus     --c-namespace tdbus_config       --interface-prefix de.tahifi.Configuration. ../de_tahifi_configuration.xml
 $CODEGEN --generate-c-code=credentials_dbus       --c-namespace tdbus_credentials  --interface-prefix de.tahifi.Credentials.  ../de_tahifi_credentials.xml
 $CODEGEN --generate-c-code=dcpd_dbus              --c-namespace tdbus_dcpd         --interface-prefix de.tahifi.Dcpd.         ../de_tahifi_dcpd.xml
@@ -24,6 +25,7 @@ $CODEGEN --generate-c-code=upnp_media_dbus        --c-namespace tdbus_upnp      
 
 $DOCEXTRACT -i ../com_intel_dleynaserver.xml -o com_intel_dleynaserver.md -H com_intel_dleynaserver.h -c tdbus_dleynaserver -s com.intel.dLeynaServer. -n dLeyna
 $DOCEXTRACT -i ../de_tahifi_airable.xml      -o de_tahifi_airable.md      -H de_tahifi_airable.h      -c tdbus              -s de.tahifi.              -n Airable
+$DOCEXTRACT -i ../de_tahifi_artcache.xml     -o de_tahifi_artcache.md     -H de_tahifi_artcache.h     -c tdbus_artcache     -s de.tahifi.ArtCache.     -n 'Cover Art Cache'
 $DOCEXTRACT -i ../de_tahifi_configuration.xml -o de_tahifi_configuration.md -H de_tahifi_configuration.h -c tdbus_config    -s de.tahifi.Configuration. -n Configuration
 $DOCEXTRACT -i ../de_tahifi_credentials.xml  -o de_tahifi_credentials.md  -H de_tahifi_credentials.h  -c tdbus_credentials  -s de.tahifi.Credentials.  -n Credentials
 $DOCEXTRACT -i ../de_tahifi_dcpd.xml         -o de_tahifi_dcpd.md         -H de_tahifi_dcpd.h         -c tdbus_dcpd         -s de.tahifi.Dcpd.         -n DCPD
