9e2b3670e748a7f6d43fecde5be6a3bd3878c1dc
Robert Tiemann <r.tiemann@ta-hifi.com>
2017-09-07 09:28:40 +0200

    D-Bus introspection file for systemd manager.
    The methods we are primarily interested in are those for unit
    management (org.freedesktop.systemd1.Manager.StartUnit,
    org.freedesktop.systemd1.Manager.StopUnit, etc.).
    
    Change-Id: Ic4e0dee1c89693e4392795df7915bd94afe857e6

--- test_doxygen.sh
+++ test_doxygen.sh
@@ -22,6 +22,7 @@ $CODEGEN --generate-c-code=audiopath_dbus         --c-namespace tdbus_aupath    
 $CODEGEN --generate-c-code=streamplayer_dbus      --c-namespace tdbus_splay        --interface-prefix de.tahifi.Streamplayer. ../de_tahifi_streamplayer.xml
 $CODEGEN --generate-c-code=connman_dbus           --c-namespace tdbus_connman      --interface-prefix net.connman.            ../net_connman.xml
 $CODEGEN --generate-c-code=logind_dbus            --c-namespace tdbus_logind       --interface-prefix org.freedesktop.login1. ../org_freedesktop_login1.xml
+$CODEGEN --generate-c-code=systemd_dbus           --c-namespace tdbus_systemd      --interface-prefix org.freedesktop.systemd1. ../org_freedesktop_systemd1.xml
 $CODEGEN --generate-c-code=upnp_media_dbus        --c-namespace tdbus_upnp         --interface-prefix org.gnome.UPnP.         ../org_gnome_upnp.xml
 
 $DOCEXTRACT -i ../com_intel_dleynaserver.xml -o com_intel_dleynaserver.md -H com_intel_dleynaserver.h -c tdbus_dleynaserver -s com.intel.dLeynaServer. -n dLeyna
@@ -38,6 +39,7 @@ $DOCEXTRACT -i ../de_tahifi_audiopath.xml    -o de_tahifi_audiopath.md    -H de_
 $DOCEXTRACT -i ../de_tahifi_streamplayer.xml -o de_tahifi_streamplayer.md -H de_tahifi_streamplayer.h -c tdbus_splay        -s de.tahifi.Streamplayer. -n Streamplayer
 $DOCEXTRACT -i ../net_connman.xml            -o net_connman.md            -H net_connman.h            -c tdbus_connman      -s net.connman.            -n Connman
 $DOCEXTRACT -i ../org_freedesktop_login1.xml -o org_freedesktop_logind.md -H org_freedesktop_logind.h -c tdbus_logind       -s org.freedesktop.login1. -n Logind
+$DOCEXTRACT -i ../org_freedesktop_systemd1.xml -o org_freedesktop_systemd.md -H org_freedesktop_systemd.h -c tdbus_systemd  -s org.freedesktop.systemd1. -n Systemd
 $DOCEXTRACT -i ../org_gnome_upnp.xml         -o org_gnome_upnp.md         -H org_gnome_upnp.h         -c tdbus_upnp         -s org.gnome.UPnP.         -n UPnP
 
 rm *.c
