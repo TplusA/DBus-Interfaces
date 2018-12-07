a2e477567c3dc5fda0fb28963be2623ea9cd031c
Robert Tiemann <r.tiemann@ta-hifi.com>
2018-12-07 11:26:56 +0100

    Interfaces provided by Gerbera.
    Change-Id: I6381b980ecc81a0f1dcfbead38cd6e37176de227

--- test_doxygen.sh
+++ test_doxygen.sh
@@ -26,6 +26,7 @@ $CODEGEN --generate-c-code=connman_dbus           --c-namespace tdbus_connman   
 $CODEGEN --generate-c-code=logind_dbus            --c-namespace tdbus_logind       --interface-prefix org.freedesktop.login1. ../org_freedesktop_login1.xml
 $CODEGEN --generate-c-code=systemd_dbus           --c-namespace tdbus_systemd      --interface-prefix org.freedesktop.systemd1. ../org_freedesktop_systemd1.xml
 $CODEGEN --generate-c-code=upnp_media_dbus        --c-namespace tdbus_upnp         --interface-prefix org.gnome.UPnP.         ../org_gnome_upnp.xml
+$CODEGEN --generate-c-code=gerbera_dbus           --c-namespace tdbus              --interface-prefix io.                     ../io_gerbera.xml
 
 $DOCEXTRACT -i ../com_intel_dleynaserver.xml -o com_intel_dleynaserver.md -H com_intel_dleynaserver.h -c tdbus_dleynaserver -s com.intel.dLeynaServer. -n dLeyna
 $DOCEXTRACT -i ../de_tahifi_airable.xml      -o de_tahifi_airable.md      -H de_tahifi_airable.h      -c tdbus              -s de.tahifi.              -n Airable
@@ -45,6 +46,7 @@ $DOCEXTRACT -i ../net_connman.xml            -o net_connman.md            -H net
 $DOCEXTRACT -i ../org_freedesktop_login1.xml -o org_freedesktop_logind.md -H org_freedesktop_logind.h -c tdbus_logind       -s org.freedesktop.login1. -n Logind
 $DOCEXTRACT -i ../org_freedesktop_systemd1.xml -o org_freedesktop_systemd.md -H org_freedesktop_systemd.h -c tdbus_systemd  -s org.freedesktop.systemd1. -n Systemd
 $DOCEXTRACT -i ../org_gnome_upnp.xml         -o org_gnome_upnp.md         -H org_gnome_upnp.h         -c tdbus_upnp         -s org.gnome.UPnP.         -n UPnP
+$DOCEXTRACT -i ../io_gerbera.xml             -o io_gerbera.md             -H io_gerbera.h             -c tdbus              -s io.                     -n 'Gerbera UPnP server'
 
 rm *.c
 
