f82ca714f071e71196b5972d7081f84d8dc71276
Robert Tiemann <r.tiemann@ta-hifi.com>
2023-02-14 09:45:55 +0100

    combine_iface_pages: Script for avoiding Doxygen warnings.
    There is a pretty stupid "feature" in Doxygen which emits a warning
    for multiple uses of \page with the same page identifier. It does the
    right thing: merge the pages into a single page. And it emits a
    warning, which is always annoying in CI.
    
    According to the Doxggen issue tracker, this warning should have been
    removed in recent versions, but it's still there. So we are doing it
    manually just to work around the warning.
    
    Change-Id: Ic8e17b6cee7049fdf03ac7a562910e07bb81578c

---
+++ combine_iface_pages.sh
@@ -0,0 +1,59 @@
+#! /bin/sh
+
+set -eu
+
+usage()
+{
+    echo "Usage: ${SCRIPTNAME} -toc <TOCFILE> -ifaces <IFACESFILE> <INPUTFILE> [<INPUTFILES>]"
+    exit 1
+}
+
+SCRIPTNAME="$0"
+
+if test $# -lt 5
+then
+    usage
+fi
+
+TOCFILE=''
+IFACESFILE=''
+
+while test $# -gt 0
+do
+    case "$1"
+    in
+        -toc)
+            TOCFILE="$2"
+            shift
+            shift
+            ;;
+        -ifaces)
+            IFACESFILE="$2"
+            shift
+            shift
+            ;;
+        -*)
+            usage
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+
+if test "x${TOCFILE}" = x || test "x${IFACESFILE}" = x
+then
+    usage
+fi
+
+echo '\page dbus_interfaces D-Bus interface documentation' >"${TOCFILE}"
+echo -n '' >"${IFACESFILE}"
+
+shift
+shift
+
+for f in "$@"
+do
+    tail -n +2 "$f" | sed -n '/^\\page /q;p' >>"${TOCFILE}"
+    tail +"$(grep -m 2 -n '^\\page ' "$f" | tail -1 | cut -f 1 -d :)" "$f" >>"${IFACESFILE}"
+done
